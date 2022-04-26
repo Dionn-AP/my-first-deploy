5f1985e259cda5575e9e32b5de67c2b7a8aa106c
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-04-25 22:19:15 -0300

    mudando configuracoes da api

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'ec2-54-80-122-11.compute-1.amazonaws.com',
-        user: 'ovzgripvfzdtag',
-        password: '8726baf21dd7e247c932cf3266310951bdf6e4b728627767c58903e9204654c0',
-        database: 'd41gtar2e12u8a',
+        host: 'ec2-52-86-56-90.compute-1.amazonaws.com',
+        user: 'xcvkixvypcdpay',
+        password: '78afe089aa5127e07ecb342bd81c918e7015ef51d25806864029a738a530d64f',
+        database: 'dco5u9epnqtl1t',
         port: 5432,
         ssl: {
             rejectUnauthorized: false
